e3639be0dd10a6fb8708012607f09cb269ba328b
NicolasET <nicolaset98@gmail.com>
2023-09-16 12:36:34 -0500

    User signin without jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from 'src/users/schemas/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
 import { AuthDto } from './dto/auth.dto';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  signIn(authDto: AuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+  ) {}
+
+  async signIn(authDto: AuthDto) {
+    const { password, username } = authDto;
+    const user = await this.userModel.findOne({ username: username });
+    if (!user) {
+      throw new UnauthorizedException('Usuario o contraseña incorrecta.');
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Usuario o contraseña incorrecta.');
+    }
   }
 }
